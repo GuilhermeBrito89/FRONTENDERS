954b041fea5c14d79ff70fa544b360e6c7ccb39d
Jadi <jadiribeiro20@gmail.com>
2022-10-30 20:19:24 -0300

    Correção do bug que não permitia o envio.

--- lista-espera/cadastrojavascript/js.js
+++ lista-espera/cadastrojavascript/js.js
@@ -21,11 +21,8 @@ enviar.addEventListener('click', function (e) {
             && regexSenha != "") {
             alert('Por favor, verifique se sua senha foi digitada corretamente.')
             e.preventDefault()
-        } else if (cep != Response) {
-            alert('Ops.. Verifique se seu CEP foi digitado corretamente')
-            e.preventDefault()
         }
-        else if (senha.value == "" || confirmsenha.value == "" || mensagem.value == "" || mail.value == "" || cep.value == "" || !mail.checkValidity() || cep.length < 8) {
+        else if (senha.value == "" || confirmsenha.value == "" || mensagem.value == "" || mail.value == "" || cep.value == "" || !mail.checkValidity() || cep.length < 8 || cep.value == "00000000") {
             alert('Por favor, verifique se todos os campos foram preenchidos corretamente.')
             e.preventDefault()
             //
