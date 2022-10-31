28c3504782c3af2955f99bb15a792c3796459020
santosfabio1 <113916185+santosfabio1@users.noreply.github.com>
2022-10-31 12:38:24 -0300

    recuperação de senha pronta

---
+++ recuperacao/javascript/atividade.js
@@ -0,0 +1,10 @@
+/*
+let btn = document.getElementsByClassName('button');
+btn.addEventListener('click', () => {
+    alert('Um link de recuperação de senha foi enviado para o seu email. Verifique também o spam');
+})
+*/
+function enviar(){
+    alert("Um link de recuperação de senha foi enviado para o seu email. Verifique também o spam");
+}
+
